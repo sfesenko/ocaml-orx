65e8b80fcf08ed4e428afb2b97453f9e1b9334c6
Sergii Fesenko <sergii_f@outlook.com>
2026-06-24 10:45:21 +0300

    update github build workflow

--- src/types/stubgen/orx_types_stubgen.ml
+++ src/types/stubgen/orx_types_stubgen.ml
@@ -1,6 +1,14 @@
 let prefix = "orx_stub"
 
 let prologue = {|
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wincompatible-pointer-types-discards-qualifiers"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
+#endif
+
 #include <orx.h>
 |}
 
